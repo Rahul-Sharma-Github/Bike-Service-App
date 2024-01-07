dd0806023cf5d84841e57c1b6f8fcf5e69dfd771
Rahul <rahulsharma3258@gmail.com>
2024-01-07 17:46:45 +0530

    Disabled the Debug Banner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Ganpati Motor Services',
       theme: ThemeData(
         primarySwatch: Colors.blue,
