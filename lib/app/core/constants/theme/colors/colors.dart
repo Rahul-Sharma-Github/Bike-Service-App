07bc5b3cf9f138da56351cb1ff47a84778703f2d
Rahul <rahulsharma3258@gmail.com>
2024-01-15 11:38:07 +0530

    A new color class ( AppColors ) for storing our App Colors created.

---
+++ lib/app/core/constants/theme/colors/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Sheet Colors
+  static const Color backSheetColor = Color(0xff3C3B3B);
+  static const Color frontSheetColor = Color(0xffFFFFFF);
+  static const Color scheduleSummarySheetColor = Color(0xffD9D9D9);
+
+  // Tab Bar Colors
+  static const Color tabBarTitleColor = Color(0xffFFFFFF);
+
+  // Input Text Box Colors
+  static const Color inputTextBoxLabelColor = Color(0xff000000);
+  static const Color inputTextBoxInnerColor = Color(0xffD9D9D9);
+
+  // Button Color
+  static const Color mainButtonColor = Color(0xff343434);
+  static const knowMoreButtonColor = Color(0xff737475);
+
+  // Card Colors
+  static const Color serviceCardColor = Color(0xffD9D9D9);
+
+  // Service Status Color
+  static const Color notStartedBoxColor = Color(0xff737475);
+  static const Color pendingOrRuningBoxColor = Color(0xffF4A227);
+  static const Color doneOrPaidBoxColor = Color(0xff3EBF01);
+}
