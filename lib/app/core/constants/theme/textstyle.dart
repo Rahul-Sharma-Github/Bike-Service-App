100df08d8d4239789b2e72e4a2038c6a205d28e4
Rahul <rahulsharma3258@gmail.com>
2024-01-15 12:17:08 +0530

    Custom TextStyles for our App Created.

---
+++ lib/app/core/constants/theme/textstyle.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyleTheme {
+  // TextStyles for Welcome Page
+  static const TextStyle welcomeHeadingTitleText = TextStyle(
+    color: Color(0xffFFFFFF),
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle welcomeHeadingSubtitleText = TextStyle(
+    color: Color(0xffFFFFFF),
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle welcomeHeadingSubtitleDescriptionText = TextStyle(
+    color: Color(0xffFFFFFF),
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+  );
+
+  // TextStyles for Page Heading
+  static const TextStyle mainTitleText = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle secondaryTitleText = TextStyle(
+    color: Color(0xff3B3B3B),
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  // TextStyles for Card Text
+  static const TextStyle cardTitleText = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle cardButtonText = TextStyle(
+    color: Color(0xffFFFFFF),
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+
+  // TextStyles for Customer Feedback Card
+  static const TextStyle customerNameText = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle customerAddressText = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  // TextStyles for Button Text
+  static const TextStyle mainButtonText = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  // TextStyles for ScheduleSummary Page
+  static const TextStyle scheduleSummaryKeyText = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 13,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle scheduleSummaryValueText = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+  );
+
+  // Global usecase TextStyles
+
+  // TextStyles for Description Text
+  static const TextStyle descriptionText = TextStyle(
+    color: Color(0xff000000),
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+}
