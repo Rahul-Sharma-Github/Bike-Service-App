9719a478f3fff2aaa570c1363102dab58d445523
Rahul <rahulsharma3258@gmail.com>
2024-01-17 10:35:40 +0530

    AssetPaths class for our App containing assets direct address for easy access.

---
+++ lib/app/core/constants/assets_path/asset_path.dart
@@ -0,0 +1,21 @@
+class AssetPaths {
+  // Welcome Page Assets Path
+  static const String bikeImage =
+      'lib/app/core/constants/assets/IntroPage/bike/bike.jpg';
+
+  // Services Page Assets Path
+  static const String brakeInspectionAndAdjustment =
+      'lib/app/core/constants/assets/ServicesPage/Brake_Inspection_and_Adjustment.png';
+
+  static const String chainMaintenance =
+      'lib/app/core/constants/assets/ServicesPage/Chain_Maintenance.png';
+
+  static const String oilAndLubrication =
+      'lib/app/core/constants/assets/ServicesPage/Oil_and_Lubrication.png';
+
+  static const String regularTuneUp =
+      'lib/app/core/constants/assets/ServicesPage/Regular_Tune_Up.png';
+
+  static const String wheelAlignment =
+      'lib/app/core/constants/assets/ServicesPage/Wheel_Alignment.png';
+}
